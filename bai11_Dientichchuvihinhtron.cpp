ce2b5518a85ecdd039dbf00c2310e0135964be96
MAY 06 <MAY 06@LAB2-20>
2024-11-18 08:42:42 +0700

    Add project files.

---
+++ bai11_Dientichchuvihinhtron.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "TINH DIEN TICH VA CHU VI HINH TRON\n";
+	double bankinh, chuvi, dientich, pi;
+	cout << "nhap ban kinh:"; cin >> bankinh;
+	cout << "pi=3,14\n";
+	pi = 3, 14;
+	chuvi = 2 * pi * bankinh;
+	dientich = pi * (bankinh * bankinh);
+
+	cout << "chu vi:" << chuvi << endl;
+	cout << "dientich:" << dientich << endl;
+	return 0;
+}
+	
